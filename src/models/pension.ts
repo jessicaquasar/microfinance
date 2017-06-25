c36f5ae0a5024a110e9681e368ddb1a0d5dcd74f
jessicaquasar <jessica.quasar@gmail.com>
2017-06-24 21:50:33 -0300

    change pension result to retrieve the correct data (#2)
    change the url to
    renamed the pension model to an understandable name

--- src/models/pension.ts
+++ src/models/pension.ts
@@ -1,4 +1,17 @@
 export interface Pension {
+  id?: number,
+  birthdate?: string,
+  gender?: string,
+  retired?: number,
+  year?: number,
+  money?: number,
+  accumulatedValue?: number,
+  monthlyContribution?: number,
+  updated_at?: Date,
+  created_at?: Date
+}
+
+export interface PensionCreate {
   birthdate?: Date
   retired?: number
   gender?: string
